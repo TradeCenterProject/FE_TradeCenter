5f13bba2623a4a1ba8fbee472a1e0d613af4bcc4
Jisu00 <jisudev00@gmail.com>
2023-01-20 21:06:20 +0900

    Feat: 필수 입력 속성 추가

--- pages/join.tsx
+++ pages/join.tsx
@@ -8,9 +8,9 @@ const JoinPage = () => {
       <Card>
         <form className="flex w-full flex-col space-y-6">
           <h2 className="text-center text-2xl font-bold">회원가입</h2>
-          <Input type="email" placeholder="이메일" />
-          <Input type="password" placeholder="비밀번호" />
-          <Input type="password" placeholder="비밀번호 확인" />
+          <Input required type="email" placeholder="이메일" />
+          <Input required type="password" placeholder="비밀번호" />
+          <Input required type="password" placeholder="비밀번호 확인" />
           <Input type="text" placeholder="회사 코드 (선택)" />
           <FormButton name="회원가입" disabled={false} />
         </form>
