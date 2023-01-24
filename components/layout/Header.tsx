7e94ad85dc1c0dbb34a4db111a42e03204f20324
Jisu00 <jisudev00@gmail.com>
2023-01-24 18:57:37 +0900

    Design: Layout position 구성 완료

---
+++ components/layout/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="w-full bg-white p-5">header</header>;
+};
+
+export default Header;
---
+++ pages/list.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+const ListPage = () => {
+  return (
+    <Layout>
+      <div>list</div>
+    </Layout>
+  );
+};
+
+export default ListPage;
