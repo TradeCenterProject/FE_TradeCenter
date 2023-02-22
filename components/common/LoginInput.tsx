de403ee6e4d2a855dc72aaf68ce07161c4202a6a
Jisu00 <jisudev00@gmail.com>
2023-02-22 22:10:21 +0900

    #23 Feat: 회원가입 Form에 react-hook-form 적용
    기존의 커스텀 훅에서 변경

--- components/common/LoginInput.tsx
+++ components/common/LoginInput.tsx
@@ -1,36 +1,27 @@
-import { ChangeEvent, useRef, useState } from "react";
-import { cls } from "@utils/styles";
+import { useRef, useState } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
 
+import { cls } from "@utils/styles";
 import PasswordOpenIcon from "@public/images/password_open_icon.svg";
 import PasswordCloseIcon from "@public/images/password_close_icon.svg";
 
 interface InputProps {
   type: string;
-  error?: string;
-  required?: boolean;
   value: string;
   placeholder: string;
-  onChangeHandler: ({ target }: ChangeEvent<HTMLInputElement>) => void;
-  [key: string]: any;
+  register: UseFormRegisterReturn;
+  error?: string;
 }
 
 const Input = ({
   type: initialType,
-  error,
-  required,
   placeholder,
   value,
-  onChangeHandler,
-  ...rest
+  register,
+  error,
 }: InputProps) => {
-  const [isTypedOnce, setIsTypedOnce] = useState(false);
   const [type, setType] = useState(initialType);
   const inputRef = useRef<HTMLInputElement>(null);
-
-  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-    onChangeHandler(e);
-    if (e.target.value.trim() !== "") setIsTypedOnce(true);
-  };
 
   const onTogglePasswordOpen = () => {
     setType(type === "password" ? "text" : "password");
@@ -43,15 +34,12 @@ const Input = ({
   return (
     <div className="relative flex flex-col">
       <input
-        ref={inputRef}
         type={type}
-        value={value}
-        {...rest}
+        {...register}
         className={cls(
           "peer w-full rounded-lg border-2 border-slate-50 bg-slate-50 px-[0.875rem] pt-5 pb-[0.1rem] text-sm outline-none focus:border-gray-500",
           initialType === "password" ? "pr-10" : ""
         )}
-        onChange={onChangeInput}
       />
       <label
         className={cls(
@@ -62,23 +50,24 @@ const Input = ({
       >
         {placeholder}
       </label>
-      <div className="absolute right-3 top-4" onClick={onTogglePasswordOpen}>
-        {initialType === "password" &&
-          (type === "password" ? (
+      {initialType === "password" && (
+        <div className="absolute right-3 top-4" onClick={onTogglePasswordOpen}>
+          {type === "password" ? (
             <PasswordOpenIcon width="16" className="pt-[1px]" />
           ) : (
             <PasswordCloseIcon width="16" />
-          ))}
-      </div>
-      {required && Boolean(error) && isTypedOnce && (
-        <span
+          )}
+        </div>
+      )}
+      {Boolean(error) && (
+        <small
           className={cls(
             "mt-2 ml-[0.5rem] text-[0.8rem]",
             Boolean(error) ? "text-red-500" : ""
           )}
         >
-          {error}
-        </span>
+          {error?.toString()}
+        </small>
       )}
     </div>
   );
--- pages/join.tsx
+++ pages/join.tsx
@@ -1,84 +1,88 @@
-import { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 import Card from "@components/common/Card";
 import FormButton from "@components/common/FormButton";
 import Input from "@components/common/LoginInput";
-import useForm from "@hooks/useForm";
+import { ERROR_MESSAGE } from "@constants/account";
+
+const regExp = {
+  email:
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+  password: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@!%*#?&])[A-Za-z\d@!%*#?&]{8,20}$/,
+};
 
 const JoinPage = () => {
-  const { values, errors, handleChange, handleSubmit } = useForm({
-    email: "",
-    password: "",
-    passwordCheck: "",
-    companyCode: "",
+  const { register, formState, handleSubmit, watch } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordCheck: "",
+      companyCode: "",
+    },
+    mode: "all",
   });
-  const [isSubmitable, setIsSubmitable] = useState(false);
+  const { errors, dirtyFields, isValid, isSubmitting } = formState;
+  const { email, password, passwordCheck, companyCode } = watch();
+  const isAllDirty =
+    dirtyFields.email && dirtyFields.password && dirtyFields.passwordCheck;
 
   const doSignUp = () => {
+    if (isSubmitting) return;
     // sign up code
-    console.log("!");
   };
-
-  const checkForm = useCallback(() => {
-    if (!errors) return false;
-
-    const invalidValues = Object.keys(values).filter((name) => {
-      return (
-        errors[name] !== undefined &&
-        (values[name] === "" || errors[name] !== "")
-      );
-    });
-
-    setIsSubmitable(!Boolean(invalidValues.length));
-  }, [errors, values]);
-
-  useEffect(() => {
-    checkForm();
-  }, [values, checkForm]);
 
   return (
     <div className="flex h-screen items-center justify-center bg-slate-50 py-20">
       <Card>
         <form
           className="flex w-full flex-col space-y-6"
-          onSubmit={(e) => handleSubmit(e, doSignUp)}
+          onSubmit={handleSubmit(doSignUp)}
         >
           <h2 className="text-center text-2xl font-bold">회원가입</h2>
           <Input
-            required
-            name="email"
             type="email"
             placeholder="이메일"
-            value={values.email}
-            onChangeHandler={handleChange}
-            error={errors?.email}
+            value={email}
+            register={register("email", {
+              required: true,
+              pattern: {
+                value: regExp.email,
+                message: ERROR_MESSAGE.email,
+              },
+            })}
+            error={errors?.email?.message}
           />
           <Input
-            required
-            name="password"
             type="password"
             placeholder="비밀번호"
-            value={values.password}
-            onChangeHandler={handleChange}
-            error={errors?.password}
+            value={password}
+            register={register("password", {
+              required: true,
+              pattern: {
+                value: regExp.password,
+                message: ERROR_MESSAGE.password,
+              },
+            })}
+            error={errors?.password?.message}
           />
           <Input
-            required
-            name="passwordCheck"
             type="password"
             placeholder="비밀번호 확인"
-            value={values.passwordCheck || ""}
-            onChangeHandler={handleChange}
-            error={errors?.passwordCheck}
+            value={passwordCheck}
+            register={register("passwordCheck", {
+              required: true,
+              validate: (value, formValues) =>
+                value === formValues.password || ERROR_MESSAGE.passwordCheck,
+            })}
+            error={errors?.passwordCheck?.message}
           />
           <Input
-            name="companyCode"
             type="text"
             placeholder="회사 코드 (선택)"
-            value={values.companyCode || ""}
-            onChangeHandler={handleChange}
+            value={companyCode}
+            register={register("companyCode")}
           />
-          <FormButton name="회원가입" disabled={!isSubmitable} />
+          <FormButton name="회원가입" disabled={!isValid || !isAllDirty} />
         </form>
       </Card>
     </div>
