410265a5aae520f4b1df4a7f0706bc0c5cf37a1e
Jisu00 <jisudev00@gmail.com>
2023-03-19 21:28:16 +0900

    #29 Feat: 엑셀 파일 읽어오는 기능 구현

--- constants/products.ts
+++ constants/products.ts
@@ -1,12 +1,14 @@
-export const PRODUCT_LIST_THEADS = [
-  "제품 코드",
-  "제품 이름",
-  "생산처",
-  "제품 분류",
-  "저장 위치",
-  "단가",
-  "수량",
-];
+export const PRODUCT_LIST_INFO = {
+  productCode: "제품 코드",
+  productName: "제품 이름",
+  productCompany: "생산처",
+  productCategory: "제품 분류",
+  location: "저장 위치",
+  price: "단가",
+  quantity: "수량",
+};
+
+export const PRODUCT_LIST_THEADS = Object.values(PRODUCT_LIST_INFO);
 
 export const SEARCH_OPTIONS = [
   "제품 코드",
