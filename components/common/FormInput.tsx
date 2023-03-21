f53c61b12c4b0c0e2129f7af29078f12023557a0
Jisu00 <jisudev00@gmail.com>
2023-03-21 18:35:42 +0900

    #31 Feat: Form으로 데이터 하나씩 추가하는 기능 구현

--- components/common/FormInput.tsx
+++ components/common/FormInput.tsx
@@ -1,11 +1,15 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
 interface FormInputProps {
   id: string;
   required?: boolean;
+  register: UseFormRegisterReturn;
 }
 
-const FormInput = ({ ...rest }: FormInputProps) => {
+const FormInput = ({ register, ...rest }: FormInputProps) => {
   return (
     <input
+      {...register}
       {...rest}
       type="text"
       className="h-fit w-full rounded-sm border border-borderColor bg-transparent py-[0.1rem] px-1 outline-none"
--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -10,19 +10,33 @@ import FormInput from "@components/common/FormInput";
 import FormItem from "@components/common/FormItem";
 import Table from "@components/common/Table";
 import Layout from "@components/layout";
+import { useForm } from "react-hook-form";
 
 interface MapType {
   [key: string]: string;
 }
 
 const ProductUploadPage = () => {
+  const { register, watch, reset } = useForm<ProductType>({
+    defaultValues: {
+      productCode: "",
+      productName: "",
+      productCompany: "",
+      productCategory: "",
+      location: "",
+      price: 0,
+      quantity: 0,
+    },
+  });
   const [dataList, setDataList] = useState<ProductType[]>([]);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleClickExcelUpload = () =>
     inputRef.current && inputRef.current.click();
 
-  const parseExcelFile = async ({ target }: ChangeEvent<HTMLInputElement>) => {
+  const addExcelFileData = async ({
+    target,
+  }: ChangeEvent<HTMLInputElement>) => {
     const fileData = target.files && target.files[0];
     if (!fileData) return;
 
@@ -37,42 +51,23 @@ const ProductUploadPage = () => {
     target.value = "";
   };
 
+  const addData = () => {
+    const row = watch();
+    setDataList((prev) => prev && [...prev, row]);
+    reset();
+  };
+
   return (
     <Layout title="물품 등록">
       <div className="space-y-10">
-        <Form id="productUpload" submittable rowCount={4}>
-          <FormItem id="productCode" label="제품 코드">
-            <FormInput id="productCode" />
-          </FormItem>
-          <FormItem id="productName" label="제품 이름">
-            <FormInput id="productName" />
-          </FormItem>
-          <FormItem id="productCategory" label="제품 분류">
-            <FormInput id="productCategory" />
-          </FormItem>
-          <FormItem id="price" label="단가">
-            <FormInput id="price" />
-          </FormItem>
-          <FormItem id="listOrder" label="No.">
-            <span className="px-1">{dataList.length + 1}</span>
-          </FormItem>
-          <FormItem id="productCompany" label="생산처">
-            <FormInput id="productCompany" />
-          </FormItem>
-          <FormItem id="location" label="저장 위치">
-            <FormInput id="location" />
-          </FormItem>
-          <FormItem id="quantity" label="수량">
-            <FormInput id="quantity" />
-          </FormItem>
-        </Form>
-        <div className="space-y-2">
-          <div className="flex justify-end gap-2">
+        <div>
+          <div className="mb-2 flex justify-end gap-2">
+            <Button color="green" value="엑셀 양식" />
             <input
               type="file"
               accept=".xlsx"
               ref={inputRef}
-              onChange={parseExcelFile}
+              onChange={addExcelFileData}
               className="hidden"
             />
             <Button
@@ -80,7 +75,65 @@ const ProductUploadPage = () => {
               value="엑셀 등록"
               handleClick={handleClickExcelUpload}
             />
-            <Button color="green" value="등록하기" />
+            <Button color="green" value="추가" handleClick={addData} />
+          </div>
+          <Form id="productUpload" rowCount={4}>
+            <FormItem id="productCode" label="제품 코드">
+              <FormInput
+                id="productCode"
+                register={register("productCode", { required: true })}
+              />
+            </FormItem>
+            <FormItem id="productName" label="제품 이름">
+              <FormInput
+                id="productName"
+                register={register("productName", { required: true })}
+              />
+            </FormItem>
+            <FormItem id="productCategory" label="제품 분류">
+              <FormInput
+                id="productCategory"
+                register={register("productCategory", { required: true })}
+              />
+            </FormItem>
+            <FormItem id="price" label="단가">
+              <FormInput
+                id="price"
+                register={register("price", {
+                  required: true,
+                  valueAsNumber: true,
+                })}
+              />
+            </FormItem>
+            <FormItem id="listOrder" label="No.">
+              <span className="px-1">{dataList.length + 1}</span>
+            </FormItem>
+            <FormItem id="productCompany" label="생산처">
+              <FormInput
+                id="productCompany"
+                register={register("productCompany", { required: true })}
+              />
+            </FormItem>
+            <FormItem id="location" label="저장 위치">
+              <FormInput
+                id="location"
+                register={register("location", { required: true })}
+              />
+            </FormItem>
+            <FormItem id="quantity" label="수량">
+              <FormInput
+                id="quantity"
+                register={register("quantity", {
+                  required: true,
+                  valueAsNumber: true,
+                })}
+              />
+            </FormItem>
+          </Form>
+        </div>
+        <div className="space-y-2">
+          <div className="flex justify-end gap-2">
+            <Button color="green" value="저장하기" />
           </div>
           <Table thead={PRODUCT_LIST_THEADS} dataList={dataList} />
         </div>
