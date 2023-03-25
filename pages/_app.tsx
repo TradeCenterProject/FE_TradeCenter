2adb21244b22cc46780b2299d6fe980f660249d6
Jisu00 <jisudev00@gmail.com>
2023-03-25 15:00:33 +0900

    Config: react-query 및 devtools 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import {
+  QueryClientProvider,
+  QueryClient,
+  Hydrate,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
